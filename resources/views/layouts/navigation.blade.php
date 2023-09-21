23873fe6fe1e5f43fe7750e748318b940d42ab37
Ottonuum <Nuumotto1@gmail.com>
2023-09-21 13:59:09 +0300

    commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div side_menu>
+    <ul>
+            <?php
+            $menuItems = array("Esmaabi", "Ennetus", "Vaimse tervise esmaabi", "Juhendid");
+            ?>
+    </ul>
+    </div>
+</body>
+</html>
+
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 w-96 fixed left-0 top-0 bottom-0">
     <div class=" h-24 flex justify-center items-center ">
         <img src="{{ asset('Logo.png') }}" alt="Logo" class="h-24">
