795b01f7dd388a90ae6f83d4dd12294cd9a42f88
Karl Andreas Mätlik <lihtsaltkasuta@gmail.com>
2023-09-22 10:30:06 +0300

    Fixes

--- database/migrations/2023_09_22_054259_esmaabi.php
+++ database/migrations/2023_09_22_054259_esmaabi.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('desctption');
             $table->string('visits');
+            $table->string('gallery');
             $table->timestamps();
         });
     }
