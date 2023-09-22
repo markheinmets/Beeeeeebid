88f9ddee5f4254bb35c848daf4aeca3631698dd2
Karl Andreas Mätlik <lihtsaltkasuta@gmail.com>
2023-09-22 12:28:46 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,50 +66,6 @@
                     <h1 class="text-5xl font-bold text-[#331B3B]">Vaimese tervise esmaabi</h1>
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
-                            <img src="{{ asset('images/Icon_1.png') }}" alt="" class="w-24">
-                            <h1 class="font-bold text-2xl mt-8 mb-4">Vaimne tervis 1</h1>
-                            <p>
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                                memiliki cabang toko Jez Salad.
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                            </p>
-                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
-                        </div>
-                        <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
-                            <img src="{{ asset('images/Icon_2.png') }}" alt="" class="w-24">
-                            <h1 class="font-bold text-2xl mt-8 mb-4">Vaimne tervis 2</h1>
-                            <p>
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                                memiliki cabang toko Jez Salad.
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                            </p>
-                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
-                        </div>
-                        <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
-                            <img src="{{ asset('images/Icon_3.png') }}" alt="" class="w-24">
-                            <h1 class="font-bold text-2xl mt-8 mb-4">Vaimne tervis 3</h1>
-                            <p>
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                                memiliki cabang toko Jez Salad.
-                                Langsung dari Kebun Sendiri yang 
-                                tersebar di setiap provinsi
-                            </p>
-                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
-                        </div>
-                    </div>
-                    <div class="w-full h-fit flex justify-end">
-                    <a href="{{ route('home') }}" class="bg-[#FFD79F] rounded-[100px] py-4 px-8 text-[#331B3B] text-lg font-semibold flex w-fit border-2 border-[#FB9333] md:border-[#FFD79F] md:hover:border-[#FB9333] transition-colors ease-in-out">Vaata veel</a>
-                    </div>
-                </section>
-                <section id="ennetavad-tegevused" class=" w-full h-fit flex flex-col gap-8">
-                    <h1 class="text-5xl font-bold text-[#331B3B]">Ennetavad tegevused</h1>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
                             <h1 class="font-bold text-2xl mt-8 mb-4">Ärevus</h1>
                             <p>
                                 On meie keha reaktsioon 
@@ -147,6 +103,50 @@
                                 tervist.
                             </p>
                             <a href="{{ route('stress') }}" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
+                        </div>
+                    </div>
+                    <div class="w-full h-fit flex justify-end">
+                    <a href="{{ route('home') }}" class="bg-[#FFD79F] rounded-[100px] py-4 px-8 text-[#331B3B] text-lg font-semibold flex w-fit border-2 border-[#FB9333] md:border-[#FFD79F] md:hover:border-[#FB9333] transition-colors ease-in-out">Vaata veel</a>
+                    </div>
+                </section>
+                <section id="ennetavad-tegevused" class=" w-full h-fit flex flex-col gap-8">
+                    <h1 class="text-5xl font-bold text-[#331B3B]">Ennetavad tegevused</h1>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
+                            <img src="{{ asset('images/Icon_1.png') }}" alt="" class="w-24">
+                            <h1 class="font-bold text-2xl mt-8 mb-4">Ennetus viis 1</h1>
+                            <p>
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                                memiliki cabang toko Jez Salad.
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                            </p>
+                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
+                        </div>
+                        <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
+                            <img src="{{ asset('images/Icon_2.png') }}" alt="" class="w-24">
+                            <h1 class="font-bold text-2xl mt-8 mb-4">Ennetus viis 2</h1>
+                            <p>
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                                memiliki cabang toko Jez Salad.
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                            </p>
+                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
+                        </div>
+                        <div class="w-full group h-full bg-white border-2 border-[#ffdfb0] md:border-[#FFF0DA] p-8 flex flex-col rounded-2xl md:hover:-translate-y-2 transition-all ease-in-out shadow-xl md:shadow-none md:hover:shadow-xl  md:hover:border-[#ffdfb0]">
+                            <img src="{{ asset('images/Icon_3.png') }}" alt="" class="w-24">
+                            <h1 class="font-bold text-2xl mt-8 mb-4">Ennetus viis 3</h1>
+                            <p>
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                                memiliki cabang toko Jez Salad.
+                                Langsung dari Kebun Sendiri yang 
+                                tersebar di setiap provinsi
+                            </p>
+                            <a href="#" class="flex mt-10 w-fit py-2 px-4 rounded-full bg-[#FB9333] md:bg-transparent md:group-hover:bg-[#FB9333] transition-all ease-in-out items-center">Loe edasi <x-heroicon-o-arrow-right class="w-5 h-5 text-black ml-1"/></a>
                         </div>
                     </div>
                     <div class="w-full h-fit flex justify-end">
